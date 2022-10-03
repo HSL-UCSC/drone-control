46fd44cf295e314d663caca3a4dcd0022a4f5d71
Zachary Owen Lamb <zolamb@ad.soe.ucsc.edu>
2022-10-02 18:29:58 -0700

    Have real-time comms working using both ble and hc12, using hc12 alone with switching is too unreliable. Might be possible not sure

--- Testing/HC12/testConn2.m
+++ Testing/HC12/testConn2.m
@@ -1,30 +1,29 @@
 clear;
 clc;
-device = serialport("COM5",9600)
+device = serialport("COM5",115200)
 flush(device)
 close all
 
-% %% Connect to the Drone via Bluetooth
-b = ble("C0286E325133"); % Make sure to get the the Drone's MAC address before running this code
+% % %% Connect to the Drone via Bluetooth
+% b = ble("C02824365530"); % Make sure to get the the Drone's MAC address before running this code
 % joy_c = characteristic(b, "00000000-0001-11E1-9AB4-0002A5D5C51B" , "00008000-0001-11E1-AC36-0002A5D5C51B") % Write w/out response
-joy_c_imu = characteristic(b, "00000000-0001-11E1-9AB4-0002A5D5C51B" , "00E00000-0001-11E1-AC36-0002A5D5C51B") % Read IMU
-
+% joy_c_imu = characteristic(b, "00000000-0001-11E1-9AB4-0002A5D5C51B" , "00E00000-0001-11E1-AC36-0002A5D5C51B") % Read IMU
+% 
 % % Arm the drone: 
 % write(joy_c, [0, 0, 0, 0, 0, 0, 2], 'uint8', "WithoutResponse") % is code running on drone different? Should send a 4 to arm?
 % java.lang.Thread.sleep(2*1000); % Java sleep is much more accurate than matlab's pause (sleep in ms)
 % write(joy_c, [22, 128, 0, 128, 128, 0, 0], 'uint8', "WithoutResponse");
-% 
-
-
-
-
-% device.BytesAvailableFcnCount = 4;
-% device.BytesAvailableFcnMode = 'byte';
-% device.BytesAvailableFcn = @instrcallback;
 
 % 
-% default = [65 84 43 68 69 70 65 85 76 84];
-% baud115200 = [65 84 43 49 49 53 50 48 48];
+% 
+% while(1)
+%     tic
+%     read(device,1,"uint8")
+%     toc
+% %     if(device.NumBytesAvailable >= 1)
+% %         read(device,1,"uint8")
+% %     end
+% end
 
 
 wTimes = [0];
@@ -33,50 +32,39 @@ startByte = 245;
 endByte = 2;
 val=0;
 
-% write(device,[startByte, 250, 1, 250, 1, endByte],"uint8")
-% read(device,1,"uint8")
-
-% write(device,[startByte, 0, 5 10, 1, endByte],"uint8")
-% value = read(device,1,"uint8")
-
-% val = [];
-% for i=1:500
-%     x = mod(i,30)+20
-%     write(device,[startByte, x, x, x, x, endByte],"uint8")
-% end
-
-
-
-
-
 count = 0;
-% while(1)
-%     if(device.NumBytesAvailable >= 5)
-%         read(device,1,"uint8")
-%     end
-% end
 readLoop = tic;
 data = zeros(1000+1,20); % For reading IMU
 timestamps = datetime(zeros(10,1), 0, 0); %a 10x1 array of datetime
 disp("Starting loop")
 for i=1:1000
     loop = tic;
-    val = mod(i,5);
+    val = 30+mod(i,10);
     write(device,[245 val 0 val val 2],"uint8")
-    [data(i,:), timestamps(i)] = read(joy_c_imu, 'latest');
-%     java.lang.Thread.sleep(25); % It takes like 100ms to switch from reading something to writing something (limitation of the hardware)
-%     read(device,1,"uint8")
+%     [data(i,:), timestamps(i)] = read(joy_c_imu, 'latest');
+    java.lang.Thread.sleep(30); % It takes like 100ms to switch from reading something to writing something (limitation of the hardware)
 
-%     if(device.NumBytesAvailable >= 1)
-%         count = count + 1;
-%         read(device,1,"uint8")
-%         r = toc(readLoop);
-%         rTimes(i) = r;
-%         readLoop = tic;
-%     end
+    if(device.NumBytesAvailable >= 1)
+        count = count + 1;
+        read(device,1,"uint8")
+        r = toc(readLoop);
+        rTimes(i) = r;
+        readLoop = tic;
+    end
     t = toc(loop);
     wTimes(i) = t;
 end
+
+for i=1:50
+    write(device,[245 0 0 0 0 2],"uint8")
+end
+
+plot(wTimes)
+
+
+
+
+
 
 % count = 0;
 % tic
---
+++ Testing/HC12/txHC12_rxBLE.m
@@ -0,0 +1,70 @@
+clear;
+clc;
+device = serialport("COM5",115200)
+flush(device)   
+close all
+
+% %% Connect to the Drone via Bluetooth
+b = ble("C02824365530"); % Make sure to get the the Drone's MAC address before running this code
+joy_c = characteristic(b, "00000000-0001-11E1-9AB4-0002A5D5C51B" , "00008000-0001-11E1-AC36-0002A5D5C51B") % Write w/out response
+joy_c_imu = characteristic(b, "00000000-0001-11E1-9AB4-0002A5D5C51B" , "00E00000-0001-11E1-AC36-0002A5D5C51B") % Read IMU
+% joy_c_imu.DataAvailableFcn = @displayCharacteristicData
+% subscribe(joy_c_imu,'notification');
+
+% Arm the drone: 
+write(joy_c, [22, 128, 0, 128, 128, 0, 4], 'uint8', "WithoutResponse");
+java.lang.Thread.sleep(2*1000); % Java sleep is much more accurate than matlab's pause (sleep in ms)
+
+
+wTimes = [0];
+rTimes = [];
+startByte = 245;
+endByte = 2;
+val=0;
+count = 0;
+readLoop = tic;
+global data;
+global timestamps;
+data = zeros(1000+1,20); % For reading IMU
+timestamps = datetime(zeros(10,1), 0, 0); %a 10x1 array of datetime
+disp("Starting loop")
+global index;
+index = 1;
+j=1;
+for i=1:1000
+    loop = tic;
+    val = 30+mod(i,10);
+    write(device,[245 val 0 val val 2],"uint8");
+    [data(i,:), timestamps(i)] = read(joy_c_imu, 'latest');
+
+    java.lang.Thread.sleep(7); % It takes like 100ms to switch from reading something to writing something (limitation of the hardware)
+    t = toc(loop);
+    wTimes(i) = t;
+end
+
+
+
+for i=1:50
+    write(device,[245 0 0 0 0 2],"uint8")
+end
+
+plot(wTimes)
+
+% unsubscribe(joy_c_imu);
+% clear joy_c_imu
+% clear b
+
+
+function displayCharacteristicData(src,evt)
+    global index;
+    global data;
+    global timestamps;
+    [d,t] = read(src, 'oldest');
+    disp(d)
+    disp(t)
+    data(index,:) = d;
+    timestamps(index) = t;
+    index=index+1;
+end
+
+
