61dc8690b96d79b9a394ed80abf1efadca2a37cc
Zachary Lamb <zolamb@ucsc.edu>
2022-11-10 18:42:48 -0800

    Normalizing rotation matrix

--- Testing/MahonyAttitudeFilter/explicit_complimentary_filter.m
+++ Testing/MahonyAttitudeFilter/explicit_complimentary_filter.m
@@ -15,7 +15,8 @@ kW = 1; % Ki in paper for wMes (keep this 1, tune it last if necessary or just t
 
 %% Parameters
 dt = 1/60;
-
+radScaleFactor = 1000000;
+% radScaleFactor = 1/1.745329252e-5; % onboard
 %% States
 % Estimated rotation matrices (body to inertial)
 R_hat = [1 0 0;
@@ -26,10 +27,12 @@ eul = rotm2eul(R_hat,'XYZ');
 % Estimated gryo bias
 bias_hat = [0,0,0];
 
+dets = det(R_hat);
+
 % Run 160Hz loop function -> ahrs_fusion_ag(&acc_ahrs, &gyro_ahrs, &ahrs);
 for i=1:datalength
     % Get latest angular rate measurement
-    omegaY = gyrRec(i,:)/1000000;
+    omegaY = gyrRec(i,:)/radScaleFactor;
     
     % Store norm of acceleration MAYBE CAN JUST INSERT ON LINE 29
     accNorm = norm(accRec(i,:));
@@ -50,14 +53,21 @@ for i=1:datalength
     
         % Update RhatDot
         RhatDot = R_hat(:,:,i)*(skewSym(omegaY - bias_hat(i+1,:)) + kP*skewSym(wMes));
-        R_hat(:,:,i+1) = R_hat(:,:,i) + RhatDot*dt;
+%         R_hat(:,:,i+1) = R_hat(:,:,i) + RhatDot*dt;
         
-        % Maybe normalize R_hat now (or the columns of R_hat)
-        eul(i+1,:) = rotm2eul(R_hat(:,:,i+1),'XYZ');
+        % Normalize R_hat (should I do this?)
+        R_hat = R_hat(:,:,i) + RhatDot*dt;
+        [u s vt] = svd(R_hat);
+        R_hat(:,:,i+1) = u * vt';
+        
     else
         R_hat(:,:,i+1) = R_hat(:,:,i);
         bias_hat(i+1,:) = bias_hat(i,:);
     end
+
+    % Record
+    eul(i+1,:) = rotm2eul(R_hat(:,:,i+1),'XYZ');
+    dets(i+1) = det(R_hat(:,:,i+1));
 end
 R_hat(:,:,end) = [];
 bias_hat(end,:) = [];
@@ -65,6 +75,7 @@ eul(end,:) = [];
 
 figure()
 plot(bias_hat)
+plot(dets)
 
 figure()
 hold on;
