0be3bfd20381521e2302503aad51b07c5b3e16cf
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2023-12-30 18:54:08 -0800

    PID class implementation

---
+++ PID/PositionController.m
@@ -0,0 +1,36 @@
+classdef PositionController
+
+    properties
+        x_pid PID
+        y_pid PID
+        z_pid PID
+    end
+
+    methods
+
+        function obj = PositionController(x_gains, y_gains, z_gains, sample_frequency, cutoff_frequency)
+
+            arguments
+                x_gains Gains = Gains(300, 15, 300);
+                y_gains Gains = Gains(300, 15, 300);
+                z_gains Gains = Gains(250, 30, 120);
+
+                sample_frequency int = 60;
+                cutoff_frequency int = 10;
+            end
+
+            obj.x_pid = PID(x_gains.kp, x_gains.ki, x_gains.kd, sample_frequency, cutoff_frequency);
+            obj.y_pid = PID(y_gains.kp, y_gains.ki, y_gains.kd, sample_frequency, cutoff_frequency);
+            obj.z_pid = PID(z_gains.kp, z_gains.ki, z_gains.kd, sample_frequency, cutoff_frequency);
+        end
+
+        function [ax, ay, az] = control(target_state, current_state, dt)
+
+            [ax, ~] = obj.x_pid.control(target_state(1), current_state(1), dt);
+            [ay, ~] = obj.y_pid.control(target_state(2), current_state(2), dt);
+            [az, ~] = obj.z_pid.control(target_state(3), current_state(3), dt);
+        end
+
+    end
+
+end
