6a5bfd4712bb60959733a89c60f5ac87fd09a5cd
Ryan <ryarodri@ucsc.edu>
2024-01-11 15:19:27 -0800

    localizer interface must be a handle
    only expose part of the interface for debug

--- Localization/+Interfaces/Localizer.m
+++ Localization/+Interfaces/Localizer.m
@@ -1,4 +1,4 @@
-classdef (Abstract) Localizer
+classdef (Abstract) Localizer < handle
 
     % Interface to a localization provider
     %  A localization provider is any source that can provide position data on a simulation entity.
@@ -7,8 +7,8 @@ classdef (Abstract) Localizer
     methods (Abstract)
         % TODO: strong preference for t at the end
         [t, x, y, z, phi, theta, psi] = get_position(obj, id, filtered)
-        [t, x_dot, y_dot, z_dot, phi_dot, theta_dot, psi_dot] = get_velocity(obj, id, filtered)
-        [t, x, y, z, phi, theta, psi, x_dot, y_dot, z_dot, phi_dot, theta_dot, psi_dot] = get_state(obj, id, filtered)
+        % [t, x_dot, y_dot, z_dot, phi_dot, theta_dot, psi_dot] = get_velocity(obj, id, filtered)
+        % [t, x, y, z, phi, theta, psi, x_dot, y_dot, z_dot, phi_dot, theta_dot, psi_dot] = get_state(obj, id, filtered)
         shutdown()
     end
 
