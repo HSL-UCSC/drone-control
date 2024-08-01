3bcdeb3e5f95279c0cccada60ff52aa7b01640a4
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-07-31 17:29:11 -0700

    wip

--- PID/PID.m
+++ PID/PID.m
@@ -1,46 +1,45 @@
 classdef PID < handle
-
+    
     properties
         kp
         ki
         kd
-        max
-        min
+        saturate_min
+        saturate_max
         current_error
         previous_error
         cumulative_error
-        
         v_current_error
     end
-
+    
     methods
-
-        function obj = PID(kp, ki, kd, min, max, sample_frequency, cutoff_frequency)
-
+        
+        function obj = PID(kp, ki, kd, saturate_min, saturate_max, sample_frequency, cutoff_frequency)
+            
             arguments
                 kp
                 ki
                 kd
-                min
-                max
+                saturate_min
+                saturate_max
                 sample_frequency
                 cutoff_frequency
             end
-
+            
             obj.kp = kp;
             obj.ki = ki;
             obj.kd = kd;
-
+            
             obj.current_error = 0;
             obj.previous_error = 0;
             obj.v_current_error = 0;
             obj.cumulative_error = 0;
-            obj.min = min;
-            obj.max = max;
-
+            obj.saturate_min = saturate_min;
+            obj.saturate_max = saturate_max;
+            % todo: add simpler digital LPF implementation, and add a filter parameter
             % obj.filter = Filter.lpf_2_init(sample_frequency, cutoff_frequency, 0.0);
         end
-
+        
         function reset(obj)
             obj.current_error = 0;
             obj.previous_error = 0;
@@ -49,29 +48,25 @@ classdef PID < handle
             
         end
         
-        function [target_acceleration, pid_outputs] = control(obj, target_state, current_state, dt)
+        function [output, component_outputs] = control(obj, target_state, current_state, dt)
             
-            % Proportional
             obj.current_error = target_state - current_state;
             pid_p = obj.kp * obj.current_error;
             
-            % Derivative
+            obj.cumulative_error = obj.cumulative_error + obj.ki*dt*(obj.current_error);
+            pid_i = obj.cumulative_error;
+            
             derivative = (obj.current_error - obj.previous_error)/dt;
             pid_d = obj.kd * derivative;
             
-            % TODO: saturate integral term
-            %Integral
-            obj.cumulative_error = obj.cumulative_error + obj.ki*dt*(obj.current_error);
-            pid_i = obj.cumulative_error;
-
-            % Store previous error
+            
             obj.previous_error = obj.current_error;
-
+            
             % Final PID result
-            pid_outputs = [pid_p, pid_i, pid_d];
-            target_acceleration = pid_p + pid_i + pid_d;
-            target_acceleration = min(max(obj.min, target_acceleration), obj.max); 
+            component_outputs = [pid_p, pid_i, pid_d];
+            output = pid_p + pid_i + pid_d;
+            output = min(max(obj.saturate_min, target_acceleration), obj.saturate_max);
         end
     end
-
+    
 end
--- Vehicle/Quadsim.m
+++ Vehicle/Quadsim.m
@@ -6,61 +6,92 @@ classdef Quadsim < Vehicle
         udp
         udprx
     end
-
-    methods 
+    
+    methods (Static)
+        function quadsim = from_yaml(yaml)
+            % Add path to the YAML parser library (update the path accordingly)
+            
+            % Example of a YAML structure:
+            % key1:
+            %   - item1
+            %   - item2
+            % key2:
+            %   - item3
+            %   - item4
+            
+            % Specify the key you want to iterate over
+            key = 'key1';
+            
+            % Check if the key exists in the YAML data
+            if isfield(yaml, key)
+                items = yamlData.(key);
+                
+                % Iterate over the items under the key
+                for i = 1:length(items)
+                    item = items{i};
+                    fprintf('Item %d: %s\n', i, item);
+                end
+            else
+                fprintf('Key "%s" not found in the YAML file.\n', key);
+            end
+            
+        end
+    end
+    methods
         function obj = Quadsim(obj, ip, port)
             if nargin == 2
                 obj.ip = ip;
                 obj.port = port;
             else
-                obj.ip = '127.0.0.1';
-                obj.port = 25000;
+              obj.ip = '127.0.0.1';
+              obj.port = 25000;
             end
-
+            
             % Define the remote IP and port
             obj.udp = udpport('datagram','IPV4');
             % obj.udprx = udpport('Datagram', 'LocalPort', 25001);
-
+            
         end
-
+        
+        % todo: load initial conditions file
         function obj = initialize(obj)
         end
         
         % cyber system
         function mode = control_mode(obj)
         end
-
+        
         % todo: this port is going to be used to mock the localization
         % interface, get this from estimated or something
         % get vehicle state 12 vector
         function [x, y, z, u, v, w, phi, theta, psi, p, q, r] = state(obj)
-            if obj.udprx.NumDatagramsAvailable > 0
-                res = read(obj.udprx, 1, 'double');
-                % disp(res.Data)
-                % [p, q, r, phi, theta, psi, u, v, w, x, y, z] = res.Data;
-                % disp([p, q, r, phi, theta, psi, u, v, w, x, y, z])
-                if length(res) >= 1
-                    data = typecast(res.Data(1:24), 'double');
-                    [p, q, r, phi, theta, psi, u, v, w, x, y, z] = deal(data(1), data(2), data(3), data(4), data(5), data(6), data(7), data(8), data(9), data(10), data(11), data(12)) ;
-                end
-            
+          if obj.udprx.NumDatagramsAvailable > 0
+            res = read(obj.udprx, 1, 'double');
+            % disp(res.Data)
+            % [p, q, r, phi, theta, psi, u, v, w, x, y, z] = res.Data;
+            % disp([p, q, r, phi, theta, psi, u, v, w, x, y, z])
+            if length(res) >= 1
+              data = typecast(res.Data(1:24), 'double');
+              [p, q, r, phi, theta, psi, u, v, w, x, y, z] = deal(data(1), data(2), data(3), data(4), data(5), data(6), data(7), data(8), data(9), data(10), data(11), data(12)) ;
             end
+            
+          end
         end
-
+        
         function control(obj, phi, theta, psi, thrust)
-            % Define the data to be sent (4 values)
-            data = [phi, theta, psi, thrust];
-            dataToSend = typecast(double(data), 'uint8');
-            write(obj.udp, dataToSend, 'uint8', obj.ip, obj.port);
+          % Define the data to be sent (4 values)
+          data = [phi, theta, psi, thrust];
+          dataToSend = typecast(double(data), 'uint8');
+          write(obj.udp, dataToSend, 'uint8', obj.ip, obj.port);
         end
-
+        
         function armed = arm(obj)
-            armed = 1;
+          armed = 1;
         end
-
+        
         function disarmed = disarm(obj)
-            disarmed=1;
+          disarmed=1;
         end
-
+        
     end
 end
