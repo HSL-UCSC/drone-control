25276f46faa3d6d2da1af5a3fe5fd90854399a97
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-07-31 22:37:59 -0700

    add initial setup script

---
+++ setup.m
@@ -0,0 +1,14 @@
+if not(isfolder('../vision'))
+    !git clone https://github.com/hsl-UCSC/vision.git ../vision/
+end
+if not(isfolder('../cybertx'))
+    !git clone https://github.com/friend0/cybertx.git ../cybertx/
+end
+
+% todo: change to the correct branches/tags
+
+% Add dependencies to the path
+addpath('../vision/clients/matlab/MotionCapture');
+
+% Save the path for future sessions
+savepath;
