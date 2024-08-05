514e4514fffa3179fd2dadf3215e756b2192e09e
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-08-05 13:06:02 -0700

    Quadsim localizer uses data from UDP callback on vehicle

---
+++ +Localization/QuadsimMotionCapture.m
@@ -0,0 +1,45 @@
+classdef QuadsimMotionCapture <  Interfaces.Localizer & handle
+    
+    properties
+        vehicles;
+    end
+    
+    methods
+        
+        
+        
+        function obj = QuadsimMotionCapture(vehicles)
+            obj.vehicles = dictionary();
+            for i = 1:length(vehicles)
+                obj.vehicles(vehicles(i).id) = vehicles(i);
+            end
+        end
+        
+        function initialize(obj)
+        end
+        
+        function success = shutdown(obj)
+          obj.client.Uninitialize();
+          success = true;
+        end
+        
+        function register_vehicle(obj, vehicle)
+          obj.vehicles(vehicle.id) = vehicle;
+        end
+        
+        function pose = get_pose(obj, subject, segment, filtered)
+          arguments
+            obj
+            subject
+            filtered = false
+          end
+          udp = obj.vehicles(subject).udprx;
+          pose.translation = udp.UserData.translation;
+          pose.rotation = udp.UserData.rotation;
+          pose.velocity = udp.UserData.velocity;
+        end
+        
+    end
+    
+end
+
